6949348f55998e774b028966db06b19b63374a68
Visard123 <92378096+Visard123@users.noreply.github.com>
2021-11-17 23:41:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+for( let i=0; i<=100; i++){
+console.log(i)
+if (i%3==0 && i>0) {
+    console.log(`Fizz`)
+}
+else if (i%5==0 && i>0 ) {
+    console.log("Buzz")
+   
+}
+else if(i%15==0 && i>0){
+    console.log(`FizzBuzz`)
+}
+}
+
+
+
