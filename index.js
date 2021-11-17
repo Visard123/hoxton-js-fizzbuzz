39807163cea4e7adc38a929109ae2af1082dfb0e
Visard123 <92378096+Visard123@users.noreply.github.com>
2021-11-18 00:01:10 +0100

    order changed to show fizzbuzz and console

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
 for( let i=0; i<=100; i++){
-console.log(i)
-if (i%3==0 && i>0) {
-    console.log(`Fizz`)
+
+if(i%15===0  && i>0){
+    console.log(`FizzBuzz`)
 }
-else if (i%5==0 && i>0 ) {
+
+
+else if (i%5===0 && i>0 ) {
     console.log("Buzz")
    
 }
-else if(i%15==0 && i>0){
-    console.log(`FizzBuzz`)
+
+else if (i%3===0 && i>0) {
+
+    console.log(`Fizz`)
 }
+else{
+    console.log(i)  
 }
+
+}
+
 
 
 
